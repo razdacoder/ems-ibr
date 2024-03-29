5d2302fb8bea152938c9ad36e86472a34f374762
Ramon Rasheed <ramonrash2@gmail.com>
2024-03-29 10:26:28 +0100

    Added all pages

--- ems/urls.py
+++ ems/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("login/", view=views.login_view, name="login"),
     path("dashboard/", view=views.dashboard, name="dashboard"),
     path("departments/", view=views.departments, name="department"),
+    path("halls/", view=views.halls, name="halls"),
+    path("timetable/", view=views.timetable, name="timetable"),
+    path("distribution/", view=views.distribution, name="distribution"),
+    path("allocation/", view=views.allocation, name="allocation"),
+    path("manage-users/", view=views.manage_users, name="manage_users"),
 ]
--- ems/views.py
+++ ems/views.py
@@ -27,3 +27,48 @@ def departments(request):
         template_name = "dashboard/departments.html"
 
     return render(request, template_name=template_name)
+
+
+def halls(request):
+    if request.htmx:
+        template_name = "dashboard/pages/halls.html"
+    else:
+        template_name = "dashboard/halls.html"
+
+    return render(request, template_name=template_name)
+
+
+def timetable(request):
+    if request.htmx:
+        template_name = "dashboard/pages/timetable.html"
+    else:
+        template_name = "dashboard/timetable.html"
+
+    return render(request, template_name=template_name)
+
+
+def distribution(request):
+    if request.htmx:
+        template_name = "dashboard/pages/distribution.html"
+    else:
+        template_name = "dashboard/distribution.html"
+
+    return render(request, template_name=template_name)
+
+
+def allocation(request):
+    if request.htmx:
+        template_name = "dashboard/pages/allocation.html"
+    else:
+        template_name = "dashboard/allocation.html"
+
+    return render(request, template_name=template_name)
+
+
+def manage_users(request):
+    if request.htmx:
+        template_name = "dashboard/pages/manage-users.html"
+    else:
+        template_name = "dashboard/manage-users.html"
+
+    return render(request, template_name=template_name)
