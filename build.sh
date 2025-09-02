89958f06f05640482b683c0fb9ac2fd6b592c24b
Ramon Rasheed <ramonrash2@gmail.com>
2025-09-02 09:43:04 +0100

    build: add makemigrations step for ems app before migrate
    The ems app migrations were being skipped during deployment since makemigrations wasn't run. This ensures all migrations are properly generated before applying them.

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
+python manage.py makemigrations ems
 python manage.py migrate ems
 python manage.py migrate
